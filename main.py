71d8a7863fe698f4858d9eeef36ccf00a1c7fdfd
visalnaqvi <74440293+visalnaqvi@users.noreply.github.com>
2023-12-10 22:43:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ cap = cv2.VideoCapture(0)
 
 # Initializing the face detector and landmark detector
 detector = dlib.get_frontal_face_detector()
+
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 # status marking for current state
